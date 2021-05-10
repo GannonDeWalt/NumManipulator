fc0078b4cd89b57cf47c2ffd626792b6dbadf019
GannonDeWalt <gannondew@gmail.com>
2021-05-10 15:26:31 -0500

    added MyInt and math

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace NumManipulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int myInt = 12;
+            Console.WriteLine(myInt);
+            myInt = (myInt^2) + 14 -3;
+            Console.WriteLine(myInt);
+        }
+    }
+}
